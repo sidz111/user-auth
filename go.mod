b39f6e28d7ac39bf623b52ebc73b37872487f690
sidz111 <sssurwade2212@gmail.com>
2026-03-17 00:49:05 +0530

    worked on service layer

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.6
 
 require (
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.49.0
 	gorm.io/driver/mysql v1.6.0
 	gorm.io/gorm v1.31.1
 )
@@ -13,5 +14,5 @@ require (
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/text v0.20.0 // indirect
+	golang.org/x/text v0.35.0 // indirect
 )
---
+++ service/user_service.go
@@ -0,0 +1,50 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/sidz111/user-auth/model"
+	"github.com/sidz111/user-auth/repository"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserService interface {
+	CreateUser(ctx context.Context, user *model.User) error
+	GetUser(ctx context.Context, id uint) (*model.User, error)
+	GetAllUsers(ctx context.Context) ([]*model.User, error)
+	UpdateUser(ctx context.Context, user *model.User) error
+	DeleteUser(ctx context.Context, id uint) error
+}
+
+type userService struct {
+	repo repository.UserRepository
+}
+
+func NewUserRepository(repo repository.UserRepository) UserService {
+	return &userService{repo: repo}
+}
+
+func (s *userService) CreateUser(ctx context.Context, user *model.User) error {
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPass)
+	return s.repo.CreateUser(ctx, user)
+}
+func (s *userService) GetUser(ctx context.Context, id uint) (*model.User, error) {
+	if id <= 0 {
+		return nil, fmt.Errorf("ID should be positive")
+	}
+	return s.repo.GetUser(ctx, id)
+}
+func (s *userService) GetAllUsers(ctx context.Context) ([]*model.User, error) {
+	return s.repo.GetAllUsers(ctx)
+}
+func (s *userService) UpdateUser(ctx context.Context, user *model.User) error {
+	return s.repo.UpdateUser(ctx, user)
+}
+func (s *userService) DeleteUser(ctx context.Context, id uint) error {
+	return s.repo.DeleteUser(ctx, id)
+}
