86412b19110cff180594958e86bca14dcbb22611
sidz111 <sssurwade2212@gmail.com>
2026-03-17 12:09:28 +0530

    generate jwt token

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.6
 
 require (
 	github.com/gin-gonic/gin v1.12.0
+	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.49.0
 	gorm.io/driver/mysql v1.6.0
---
+++ utils/jwt.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
+)
+
+func GenerateJWT(username string, id uint) (string, error) {
+	if err := godotenv.Load(); err != nil {
+		return "failed to create jwt", err
+	}
+
+	claims := jwt.MapClaims{
+		"username": username,
+		"user_id":  id,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(os.Getenv("SECRET_KEY"))
+	if err != nil {
+		return "failed to create jwt", err
+	}
+	return tokenString, nil
+}
